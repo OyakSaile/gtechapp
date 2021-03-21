f3946114f3d8ca46d7c31c6605257ef44ac36139
Kayo Elias <contatodevkayo@gmail.com>
2021-03-21 15:25:22 -0300

    Rotas Adicionadas e Global CSS Ajustado e linkado

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,19 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
+  IonImg
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Tab4 from './pages/Tab4';
+import Tab5 from './pages/Tab5';
+import HomePage from './pages/HomePage/HomePage'
+import EnterPrisePage from './pages/EnterPrisePage/EnterPrisePage'
+import { faHome, faUsers, faList, faShareAlt, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import whatsappIcon from './assets/icons/whatsapp.svg'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -32,37 +39,57 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import './theme/global.css'
+
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path="/HomePage">
+            <HomePage />
           </Route>
           <Route exact path="/tab2">
-            <Tab2 />
+            <EnterPrisePage />
           </Route>
           <Route path="/tab3">
             <Tab3 />
           </Route>
+          <Route path="/tab4">
+            <Tab4 />
+          </Route>
+
+          <Route path="/tab5">
+            <Tab5 />
+          </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/HomePage" />
           </Route>
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+        <IonTabBar className="ion-text-center" slot="bottom">
+          <IonTabButton tab="HomePage" href="/HomePage">
+            <FontAwesomeIcon icon={faHome} size="lg" />
+
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <FontAwesomeIcon icon={faUsers} size="lg" />
+
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+          <IonTabButton href='https://api.whatsapp.com/send?phone=5521981331302&text=Boa%20tarde%2C%20Gostaria%20de%20fazer%20um%20Or%C3%A7amento%20de%20um%20de%20seus%20servi%C3%A7os.' className="whatsappTabButton">
+            <FontAwesomeIcon icon={faPaperPlane} size="lg" />
+
+          </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab4">
+            <FontAwesomeIcon icon={faList} size="lg" />
+
+          </IonTabButton>
+
+          <IonTabButton tab="tab5" href="/tab5">
+            <FontAwesomeIcon icon={faShareAlt} size="lg" />
+
+
+
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -8,7 +8,7 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div className="container">
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>Conheça <a target="_blank" rel="noopener noreferrer" href="https://api.whatsapp.com/send?phone=5521981331302&text=Boa%20tarde%2C%20Gostaria%20de%20fazer%20um%20Or%C3%A7amento%20de%20um%20de%20seus%20servi%C3%A7os.">Entre em Contato</a></p>
     </div>
   );
 };
